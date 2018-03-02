c9141b25a170f522b32b11cb4db07c038a908036
kawase_Mac <kgbull0227@gmail.com>
2018-03-02 16:47:21 +0900

    Complategit add --all sass__gulp-project

--- sass/gulp-project/gulpfile.js
+++ sass/gulp-project/gulpfile.js
@@ -1,12 +1,32 @@
-var gulp = require('gulp');
-var compass = require('gulp-compass');
+var gulp = require('gulp'),
+    compass = require('gulp-compass'),
+    webserver = require('gulp-webserver');
 
-//compass
-gulp.task('compass', function(){
-  gulp.src('sass/**/*.scss').pipe(compass({
-    config_file: 'config.rb',
-    comments: false,
-    css: 'css/',
-    sass: 'sass/'
-  }));
+
+// webserver
+gulp.task('webserver', function () {
+    gulp.src('.')
+    .pipe(webserver({
+        host: 'localhost',
+        port: 8000,
+        livereload: true
+    }));
 });
+
+// compass
+gulp.task('compass', function(){
+    gulp.src('sass/**/*.scss')
+    .pipe(compass({
+        config_file: 'config.rb',
+        comments: false,
+        css: 'css/',
+        sass: 'sass/'
+    }));
+});
+
+//watch
+gulp.task('watch', function(){
+  gulp.watch('sass/*.scss',['compass']);
+});
+
+gulp.task('default', ['watch','webserver']);
